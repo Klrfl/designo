d22d6713f5e821b3da22d77937701c7f9b5f799d
Efraim Munthe <efrayanglain@gmail.com>
2025-07-24 16:19:41 +0700

    ui: create base styles, add index page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -8,5 +8,61 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <h1> hai</h1>;
+  return (
+    <main>
+      <header className="px-8 py-6">
+        <h1>Award-winning custom designs and digital branding solutions</h1>
+
+        <p>
+          With over 10 years in the industry, we are experienced in creating
+          fully responsive websites, app design, and engaging brand experiences.
+          Find out more about our services.
+        </p>
+
+        <button>Learn more</button>
+      </header>
+
+      <div className="p-4">
+        <h2>Web Design</h2>
+        <a href="#">View Projects</a>
+      </div>
+
+      <div className="p-4">
+        <h2>App Design</h2>
+        <a href="#">View Projects</a>
+      </div>
+
+      <div className="p-4">
+        <h2>Graphic Design</h2>
+        <a href="#">View Projects</a>
+      </div>
+
+      <div className="flex items-center gap-4">
+        <div className="p-4">
+          <h2>Passionate</h2>
+          <p>
+            Each project starts with an in-depth brand research to ensure we
+            only create products that serve a purpose. We merge art, design, and
+            technology into exciting new solutions.
+          </p>
+        </div>
+
+        <div className="p-4">
+          <h2>Resourceful</h2>
+          <p>
+            Everything that we do has a strategic purpose. We use an agile
+            approach in all of our projects and value customer collaboration. It
+            guarantees superior results that fulfill our clients’ needs.
+          </p>
+        </div>
+
+        <div className="p-4">
+          <h2> Friendly </h2>
+          We are a group of enthusiastic folks who know how to put people first.
+          Our success depends on our customers, and we strive to give them the
+          best experience a company can provide.
+        </div>
+      </div>
+    </main>
+  );
 }
