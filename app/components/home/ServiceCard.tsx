fe69f6f01b3ae1ebead7ac387e845d68174318ae
Efraim Munthe <efrayanglain@gmail.com>
2025-07-26 11:19:58 +0700

    ui: make whole card clickable

--- app/components/home/ServiceCard.tsx
+++ app/components/home/ServiceCard.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router";
+
 interface Props {
   className?: string | string[];
   image: string;
@@ -9,7 +11,7 @@ export default function ServiceCard(props: Props) {
   return (
     <div
       className={[
-        "flex flex-col justify-center items-center text-center relative col-span-full md:col-span-6 px-24 py-32 rounded-2xl overflow-hidden",
+        "col-span-full md:col-span-6 rounded-2xl overflow-hidden relative",
         props.className ?? [],
       ]
         .flat()
@@ -17,21 +19,22 @@ export default function ServiceCard(props: Props) {
     >
       <img
         src={props.image}
-        className={[
-          "absolute inset-0 size-full object-center object-cover grayscale brightness-50",
-          ...(props.className ?? []),
-        ]
-          .flat()
-          .join()}
+        className="absolute inset-0 size-full object-cover brightness-50"
         alt=""
       />
 
-      <div className="z-1 text-white uppercase">
-        <h2 className="heading-2">{props.title}</h2>
-        <a className="tracking-[5px]" href={props.link ?? "#"}>
-          View Projects
-        </a>
-      </div>
+      <Link
+        to={props.link ?? "#"}
+        className="relative flex flex-col justify-center items-center text-center px-24 py-32 size-full hover:bg-primary/50 focus-visible:bg-primary/50 transition-[background-color]"
+        aria-aria-labelledby={props.title}
+      >
+        <div className="text-white uppercase">
+          <h2 id={props.title} className="heading-2">
+            {props.title}
+          </h2>
+          <span className="tracking-[5px]">View Projects</span>
+        </div>
+      </Link>
     </div>
   );
 }
