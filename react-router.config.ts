d958b1510154d71de346bcd3caf0d159c4dd9dc4
Efraim Munthe <efrayanglain@gmail.com>
2025-07-29 11:16:56 +0700

    change build script

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,4 +4,13 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
+  prerender: [
+    "/",
+    "/about",
+    "/locations",
+    "/contact",
+    "/services/app-design",
+    "/services/web-design",
+    "/services/graphic-design",
+  ]
 } satisfies Config;
