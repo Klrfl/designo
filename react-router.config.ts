87bf87da8f8c8f4765ddc8320010f2403c681e13
Efraim Munthe <efrayanglain@gmail.com>
2025-07-29 15:54:14 +0700

    Netlify (#1)
    * prerender routes
    
    * build: change build script, add netlify vite plugin
    
    * ignore netlify folder
    
    * remove prrerender
    
    * fix build
    
    * setup tina proper
    
    * add netlify branch

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,14 +3,5 @@ import type { Config } from "@react-router/dev/config";
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: false,
-  prerender: [
-    "/",
-    "/about",
-    "/locations",
-    "/contact",
-    "/services/app-design",
-    "/services/web-design",
-    "/services/graphic-design",
-  ],
+  ssr: true,
 } satisfies Config;
